76ee7c96b3c9c2630e1841cf193974d54bc48316
Bubunyo Nyavor <kidd.bubu@gmail.com>
2026-03-08 04:42:28 +0100

    clean up

--- testproject/core/module-a/foo.go
+++ testproject/core/module-a/foo.go
@@ -3,18 +3,15 @@ package module_a
 import "fmt"
 
 func Process(input string) string {
-	_ = 3
 	result := Transform(input)
 	fmt.Println("Syntetic change 3")
 	return result
 }
 
 func Fetch() string {
-	_ = 1
 	return "data"
 }
 
 func Transform(data string) string {
-	_ = 1
 	return fmt.Sprintf("processed and transformed: %s", data)
 }
--- testproject/core/module-b/bar.go
+++ testproject/core/module-b/bar.go
@@ -3,13 +3,11 @@ package module_b
 import "fmt"
 
 func Save(data string) error {
-	_ = 1
 	fmt.Println("Saving:", data)
 	return nil
 }
 
 func Delete(id string) error {
-	_ = 1
-	fmt.Println("Deleting: ", id)
+	fmt.Println("Deleting:", id)
 	return nil
 }
