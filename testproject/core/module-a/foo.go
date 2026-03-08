61fa6c82606f8e149b3cf1ae135b111c77cf2820
Bubunyo Nyavor <kidd.bubu@gmail.com>
2026-03-08 03:59:00 +0100

    add build trigger

--- testproject/core/module-a/foo.go
+++ testproject/core/module-a/foo.go
@@ -3,15 +3,18 @@ package module_a
 import "fmt"
 
 func Process(input string) string {
+	_ = 1
 	result := Transform(input)
 	fmt.Println("Syntetic change")
 	return result
 }
 
 func Fetch() string {
+	_ = 1
 	return "data"
 }
 
 func Transform(data string) string {
+	_ = 1
 	return fmt.Sprintf("processed and transformed: %s", data)
 }
--- testproject/core/module-b/bar.go
+++ testproject/core/module-b/bar.go
@@ -3,11 +3,13 @@ package module_b
 import "fmt"
 
 func Save(data string) error {
+	_ = 1
 	fmt.Println("Saving:", data)
 	return nil
 }
 
 func Delete(id string) error {
+	_ = 1
 	fmt.Println("Deleting: ", id)
 	return nil
 }
